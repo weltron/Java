addcb584ce3ff2b93659f1cd6bf21718e8c5575e
Welt <bweltron@gmail.com>
2022-11-09 23:40:10 -0600

    Java Practise

---
+++ Java/main.java
@@ -0,0 +1,13 @@
+class Main{
+    public static void main(String[] args){
+        char vowels[] = new char[5];
+        vowels[0] = 'a';
+        vowels[1] = 'e';
+        vowels[2] = 'i';
+        vowels[3] = 'o';
+        vowels[4] = 'u';
+
+        System.out.println(Arrays.toString(vowels));
+
+    }
+}
